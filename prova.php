a2d7a59626bf5f1fdf2b3504017bbd2977389d99
paolodalex96@gmail.com <7u,txxG6QneBbw%>
2022-08-31 16:43:10 +0200

    fix: fixed homepage layout css

--- prova.php
+++ prova.php
@@ -9,42 +9,53 @@
 </head>
 <body>
 
-    <?php
-        include "config.php";
+    <div class="container">
+        <div class="column-20"></div>
+        
+        <div class="column-60">
+        
+            <?php
+                include "config.php";
 
-        $sql = "SELECT * FROM users";
-        $result = $con -> query($sql);
+                $sql = "SELECT * FROM users";
+                $result = $con -> query($sql);
 
-        if($result->num_rows > 0){
-            echo '<table>
-                    <thead>
-                        <tr>
-                            <th>id</th>
-                            <th>name</th>
-                            <th>surname</th>
-                            <th>username</th>
-                            <th>age</th>
-                            <th>email</th>
-                        </tr>
-                    </thead>
-                    <tbody>';
-            while($row = $result->fetch_assoc()){
-                echo '<tr>
-                        <td>' .$row['id']. '</td>
-                        <td>' .$row['name']. '</td>
-                        <td>' .$row['surname']. '</td>
-                        <td>' .$row['username']. '</td>
-                        <td>' .$row['age']. '</td>
-                        <td>' .$row['email']. '</td>
-                    </tr>';
-            }
-            echo '</tbody></table>';
-        }
-    ?>
+                if($result->num_rows > 0){
+                    echo '<table>
+                            <thead>
+                                <tr>
+                                    <th>id</th>
+                                    <th>name</th>
+                                    <th>surname</th>
+                                    <th>username</th>
+                                    <th>age</th>
+                                    <th>email</th>
+                                </tr>
+                            </thead>
+                            <tbody>';
+                    while($row = $result->fetch_assoc()){
+                        echo '<tr>
+                                <td>' .$row['id']. '</td>
+                                <td>' .$row['name']. '</td>
+                                <td>' .$row['surname']. '</td>
+                                <td>' .$row['username']. '</td>
+                                <td>' .$row['age']. '</td>
+                                <td>' .$row['email']. '</td>
+                            </tr>';
+                    }
+                    echo '</tbody></table>';
+                }
+            ?>
 
-    <div class="button_container">
-        <a href="addUser.php">add user</a>
+        </div>
+        <div class="column-20">
+            <div class="button_container">
+                <a href="login.php">log in</a>
+                <a href="addUser.php">add user</a>
+            </div>
+        </div>
     </div>
+
     
 </body>
 </html>
